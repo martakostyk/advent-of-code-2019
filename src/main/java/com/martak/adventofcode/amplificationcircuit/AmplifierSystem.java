b61f26f4304428904aad5fdef2a0b3c23cf59d3f
marta kostyk <martakostyk@op.pl>
2019-12-12 12:33:02 +0100

    loop amplifier system to find the biggest output signal

--- src/main/java/com/martak/adventofcode/amplificationcircuit/AmplifierSystem.java
+++ src/main/java/com/martak/adventofcode/amplificationcircuit/AmplifierSystem.java
@@ -1,36 +1,70 @@
 package com.martak.adventofcode.amplificationcircuit;
 
+import com.martak.adventofcode.utils.InputStorage;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class AmplifierSystem {
 
     private final List<Amplifier> amplifiers;
 
-    public AmplifierSystem( List<Amplifier> amplifiers) {
+    public AmplifierSystem(List<Amplifier> amplifiers) {
         this.amplifiers = amplifiers;
     }
 
     public static void main(String[] args) {
-        String phaseSettingSequence = "43210";
-        List<Amplifier> amplifiers = AmplifierFactory.getAmplifiers(phaseSettingSequence);
-        AmplifierSystem amplifierSystem = new AmplifierSystem(amplifiers);
-        int[] controllerSoftware = AmplifierSystem.getControllerSoftware();
-        String thrusterSignal = amplifierSystem.execute(controllerSoftware);
-        System.out.println("Thruster signal " + thrusterSignal);
+        List<String> phaseSequenceCombinations = AmplifierSystem.getAllPhaseSequenceCombinations();
+        int[] controllerSoftware = InputStorage.getControllerSoftware();
+        String theBiggestThrusterSignal = "";
+        for (String phaseSequence : phaseSequenceCombinations) {
+            List<Amplifier> amplifiers = AmplifierFactory.getAmplifiers(phaseSequence);
+            AmplifierSystem amplifierSystem = new AmplifierSystem(amplifiers);
+            String thrusterSignal = amplifierSystem.execute(controllerSoftware);
+            if (thrusterSignal.compareTo(theBiggestThrusterSignal) > 0) {
+                theBiggestThrusterSignal = thrusterSignal;
+            }
+        }
+        System.out.println("The biggest thruster signal " + theBiggestThrusterSignal);
     }
 
-    String execute(int[] controllerSoftware) {
+    public String execute(int[] controllerSoftware) {
         int output = 0;
         for (int i = 0; i < 5; i++) {
             Amplifier amplifier = amplifiers.get(i);
-            int[] inputSignals = new int[] {amplifier.getPhase(), output};
+            int[] inputSignals = new int[]{amplifier.getPhase(), output};
             output = Integer.valueOf(amplifier.execute(controllerSoftware, inputSignals));
         }
-
         return String.valueOf(output);
     }
 
-    static int[] getControllerSoftware() {
-        return new int[] {3,8,1001,8,10,8,105,1,0,0,21,46,67,76,101,118,199,280,361,442,99999,3,9,1002,9,4,9,1001,9,2,9,102,3,9,9,101,3,9,9,102,2,9,9,4,9,99,3,9,1001,9,3,9,102,2,9,9,1001,9,2,9,1002,9,3,9,4,9,99,3,9,101,3,9,9,4,9,99,3,9,1001,9,2,9,1002,9,5,9,101,5,9,9,1002,9,4,9,101,5,9,9,4,9,99,3,9,102,2,9,9,1001,9,5,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,99};
+    private static List<String> getAllPhaseSequenceCombinations() {
+        List<String> phaseSequenceCombinations = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i == j) {
+                    continue;
+                }
+                for (int k = 0; k < 5; k++) {
+                    if (i == k || j == k) {
+                        continue;
+                    }
+                    for (int l = 0; l < 5; l++) {
+                        if (i == l || j == l || k == l) {
+                            continue;
+                        }
+                        for (int m = 0; m < 5; m++) {
+                            if (i == m || j == m || k == m || l == m) {
+                                continue;
+                            }
+                            phaseSequenceCombinations.add(i + "" + j + "" + k + "" + l + "" + m);
+                        }
+                    }
+                }
+            }
+        }
+        return phaseSequenceCombinations;
     }
+
+
 }
