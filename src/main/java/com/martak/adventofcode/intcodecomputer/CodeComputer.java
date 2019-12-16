1c43e183d86271421683df96e790d1bcc2a3d093
marta kostyk <martakostyk@op.pl>
2019-12-16 12:28:54 +0100

    refactor itcode computer to get context as the method arg and return context to have the state of running diagnostic code

--- src/main/java/com/martak/adventofcode/intcodecomputer/CodeComputer.java
+++ src/main/java/com/martak/adventofcode/intcodecomputer/CodeComputer.java
@@ -9,34 +9,19 @@ public class CodeComputer {
 
     public static void main(String[] args) {
         int[] diagnosticTests = InputStorage.getDiagnosticTests();
-        String output = new CodeComputer()
-                .execute(diagnosticTests, new int[]{Integer.parseInt(args[0])});
-        System.out.println("Code computer output " + output);
-    }
-
-    private int findPair() {
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                int[] code = InputStorage.getInput();
-                code[1] = i;
-                code[2] = j;
-                execute(code, new int[]{0});
-                if (code[0] == 19690720) {
-                    return 100 * i + j;
-                }
-            }
-        }
-        return 0;
-    }
-
-    public String execute(int[] code, int[] inputs) {
-        StringBuilder output = new StringBuilder();
-        Context context = new Context(0, code, inputs, output.toString());
+        Context context = new Context(0, diagnosticTests, new int[]{Integer.parseInt(args[0])}, "");
         while (context.getPointer() < context.getCode().length) {
+            context = new CodeComputer().execute(context);
+        }
+        System.out.println("Code computer output " + context.getOutput());
+    }
+
+    public Context execute(Context context) {
+        String currentOutput = context.getOutput();
+        while (context.getOutput().equals(currentOutput) && context.getPointer() < context.getCode().length) {
             OpCode opCode = OpCodeFactory.getOpCode(context.getCode()[context.getPointer()]);
             context = opCode.execute(context);
-            output.append(context.getOutput());
         }
-        return context.getOutput();
+        return context;
     }
 }
--- src/test/java/com/martak/adventofcode/intcodecomputer/CodeComputerTest.java
+++ src/test/java/com/martak/adventofcode/intcodecomputer/CodeComputerTest.java
@@ -1,5 +1,6 @@
 package com.martak.adventofcode.intcodecomputer;
 
+import com.martak.adventofcode.intcodecomputer.model.Context;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -7,33 +8,33 @@ public class CodeComputerTest {
 
     @Test
     public void test1a() {
-        int input = 0;
         int[] diagnosticTests = new int[]{3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9};
-        String output = new CodeComputer().execute(diagnosticTests, new int[]{input});
-        Assert.assertEquals(Integer.valueOf(output), Integer.valueOf(0));
+        Context context = new Context(0, diagnosticTests, new int[]{0}, "");
+        context = new CodeComputer().execute(context);
+        Assert.assertEquals(Integer.valueOf(context.getOutput()), Integer.valueOf(0));
     }
 
     @Test
     public void test1b() {
-        int input = 1;
         int[] diagnosticTests = new int[]{3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9};
-        String output = new CodeComputer().execute(diagnosticTests, new int[] {input});
-        Assert.assertEquals(Integer.valueOf(output), Integer.valueOf(1));
+        Context context = new Context(0, diagnosticTests, new int[]{1}, "");
+        context = new CodeComputer().execute(context);
+        Assert.assertEquals(Integer.valueOf(context.getOutput()), Integer.valueOf(1));
     }
 
     @Test
     public void test2a() {
-        int input = 0;
         int[] diagnosticTests = new int[]{3,3,1105,-1,9,1101,0,0,12,4,12,99,1};
-        String output = new CodeComputer().execute(diagnosticTests, new int[] {input});
-        Assert.assertEquals(Integer.valueOf(output), Integer.valueOf(0));
+        Context context = new Context(0, diagnosticTests, new int[]{0}, "");
+        context = new CodeComputer().execute(context);
+        Assert.assertEquals(Integer.valueOf(context.getOutput()), Integer.valueOf(0));
     }
 
     @Test
     public void test2b() {
-        int input = 1;
         int[] diagnosticTests = new int[]{3,3,1105,-1,9,1101,0,0,12,4,12,99,1};
-        String output = new CodeComputer().execute(diagnosticTests, new int[] {input});
-        Assert.assertEquals(Integer.valueOf(output), Integer.valueOf(1));
+        Context context = new Context(0, diagnosticTests, new int[]{1}, "");
+        context = new CodeComputer().execute(context);
+        Assert.assertEquals(Integer.valueOf(context.getOutput()), Integer.valueOf(1));
     }
 }
